1e90055ec4a2900e115fff52a3a34853189eee7d
azbang <andrey@herewallet.app>
2026-04-01 17:23:05 +0300

    fix ton manifest

--- demo-ui/src/hooks/bridge.ts
+++ demo-ui/src/hooks/bridge.ts
@@ -1,8 +1,19 @@
 import { HotConnector } from "@hot-labs/kit";
-import { defaultConnectors } from "@hot-labs/kit/defaults";
 import cosmos from "@hot-labs/kit/cosmos";
+import evm from "@hot-labs/kit/evm";
+import stellar from "@hot-labs/kit/stellar";
+import solana from "@hot-labs/kit/solana";
+import ton from "@hot-labs/kit/ton";
+import near from "@hot-labs/kit/near";
 
 export const wibe3 = new HotConnector({
-  connectors: [...defaultConnectors, cosmos()],
+  connectors: [
+    near(),
+    evm(),
+    cosmos(),
+    stellar(),
+    solana(),
+    ton({ tonManifestUrl: "https://hot-dao.github.io/omni-sdk/tonconnect-manifest.json" }),
+  ],
   apiKey: "",
 });
